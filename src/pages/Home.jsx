949861446f97cf7a32f8c5802625e629f24d255e
Nadine Salem <73083314+nadinesalem@users.noreply.github.com>
2024-03-07 14:59:13 -0600

    initialized island to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 
+import Island from '../models/Island';
+
 {/*<div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
     POPUP
 </div>*/}
@@ -19,6 +21,8 @@ const Home = () => {
           <pointLight />
           <spotLight />
           <hemisphereLight />
+
+          <Island />
         </Suspense>
       </Canvas>
     </section>
